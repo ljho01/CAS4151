9416c216b0d31eaec71065ac33fcc59defba9ed5
Lee Jaeho <ljhasdr@gmail.com>
2025-04-29 16:58:36 +0900

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { metadata as siteMetadata } from "./metadata";
-import { Analytics } from "@vercel/analytics/react"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>{children}</body>
-      <Analytics />
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { HomeContent } from '@/components/HomeContent';
 import { defaultLocale } from '@/i18n';
 import { LocaleManager } from '@/components/LocaleManager';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   const [locale, setLocale] = useState(defaultLocale);
@@ -12,6 +13,8 @@ export default function Home() {
     <main>
       <LocaleManager onLocaleChange={setLocale} />
       <HomeContent locale={locale} />
+            <Analytics />
+
     </main>
   );
 } 
