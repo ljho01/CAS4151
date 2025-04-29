8a2149c9a8d887a7c7dfdbcc7083e531daabfd27
Lee Jaeho <ljhasdr@gmail.com>
2025-04-29 15:18:15 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import { useState } from 'react';
 import { HomeContent } from '@/components/HomeContent';
 import { defaultLocale } from '@/i18n';
+import { LocaleManager } from '@/components/LocaleManager';
 
 export default function Home() {
   const [locale, setLocale] = useState(defaultLocale);
 
   return (
     <main>
+      <LocaleManager onLocaleChange={setLocale} />
       <HomeContent locale={locale} />
     </main>
   );
