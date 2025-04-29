a2965a7afef008584321f47a22faa88a09d00a01
Lee Jaeho <ljhasdr@gmail.com>
2025-04-29 16:56:57 +0900

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { metadata as siteMetadata } from "./metadata";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
